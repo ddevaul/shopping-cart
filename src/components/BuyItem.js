ef9ff43d5fe30e29a6b6ed3e123e8d2691a0b922
Desi <desi.devaul@gmail.com>
2021-02-12 09:50:21 -0500

    Moved images to src directory for compatability with gh pages.

--- src/components/BuyItem.js
+++ src/components/BuyItem.js
@@ -1,15 +1,14 @@
 
 import "./style.css"
 
-const selectRightImage = () => {
 
-}
 
 export function BuyItem({ addItem, item }) { 
-
+  const images = require.context("../images", true);
+  let img = images(item.image).default;
   return (
     <div className="buy-item">
-      <img className="item-image" src={item.image} alt={`Cover of ${item.name}`}></img>
+      <img className="item-image" src={img} alt={`Cover of ${item.name}`}></img>
       {item.name}
       <button onClick={() => {
         addItem(item)
--- src/data/data.js
+++ src/data/data.js
@@ -2,21 +2,21 @@ export const data = [
   {
      "name":"Catch-22",
      "id":"6006",
-     "image":"../images/Catch22.jpg",
+     "image":"./Catch22.jpg",
   },
   {
      "name":"100 Years of Solitude",
      "id":"100",
-     "image":"../images/100yearsofsolitude.jpg"
+     "image":"./100yearsofsolitude.jpg"
   },
   {
      "name":"Player Piano",
      "id":"674734",
-     "image":"../images/playerpiano.jpg"
+     "image":"./playerpiano.jpg"
   },
   {
      "name":"Loonshots",
      "id":"7009",
-     "image":"../images/loonshots.jpg"
+     "image":"./loonshots.jpg"
   }
 ]
