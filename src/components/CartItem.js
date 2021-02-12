fb8e5966912dde598f9f1e014a27f3c5397f9e7e
Desi <desi.devaul@gmail.com>
2021-02-12 10:29:07 -0500

    Changed url's for gh pages.

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,9 @@
 export function CartItem({ item, quantity,removeItem }) {
+  const images = require.context("../images", true);
+  let img = images(item.image).default;
   return (
     <div className="cart-item">{item.name}
-      <img className="item-image" src={item.image} alt={`Cover of ${item.name}`}></img>
+      <img className="item-image" src={img} alt={`Cover of ${item.name}`}></img>
       <p>Quantity: {quantity} </p>
       <button onClick={() => {removeItem(item)}}>Remove from cart</button>
     </div>
